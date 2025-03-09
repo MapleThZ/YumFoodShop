2c95eeaa16cb5f222386b3155ef73ea6dc894d97
PiyanatK <boom.enginear@gmail.com>
2025-03-09 23:17:42 +0700

    [update] => comment redirects

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
+const nextConfig: NextConfig = {
 //   async redirects() {
 //     return [
 //       {
-//         source: '/',
-//         destination: '/dashboard',
-//         permanent: true,
+//         source: '/dashboard',
+//         destination: '/',
+//         permanent: false,
 //       }
 //     ];
 //   },
-// };
+};
 
-// export default nextConfig;
+export default nextConfig;
